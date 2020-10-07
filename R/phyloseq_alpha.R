c3b464f82803e6e3645cd9a3b3a80e224e005fcd
fconstancias <florentinconstancias@gmail.com>
2020-10-07 23:05:29 +0200

    Update phyloseq_alpha.R
    ...

--- R/phyloseq_alpha.R
+++ R/phyloseq_alpha.R
@@ -110,13 +110,9 @@ plot_alphas <- function(alphas,
     # geom_point(size=2,position=position_jitterdodge(dodge.width=0.9)) +
     ylab("Diversity index")  + xlab(NULL) + theme_light() -> p
 
-  { 
-if (facet_group == FALSE) {
-  p + facet_grid(as.formula(paste0("alphadiversiy ~ ", scales = "free_y", space = "fixed") -> p
-                            } else {
-  p + facet_grid(as.formula(paste0("alphadiversiy ~ ",paste(facet_group))), scales = "free_y", space = "fixed") -> p
+if (facet_group == FALSE) { p + facet_grid(as.formula(paste0("alphadiversiy ~ ", scales = "free_y", space = "fixed") -> p
+                            } else { p + facet_grid(as.formula(paste0("alphadiversiy ~ ",paste(facet_group))), scales = "free_y", space = "fixed") -> p
   }
-    }
 
 
 
