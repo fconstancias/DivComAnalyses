1436647c68a89dad3e892b3a786fab7651b2590f
fconstancias <florentinconstancias@gmail.com>
2021-07-20 20:46:41 +0200

    update phyloseq_add_taxa_vector
    inerit.aes FALSE to make sure any ordination could be ploted

--- R/phyloseq_beta.R
+++ R/phyloseq_beta.R
@@ -1047,8 +1047,9 @@ phyloseq_add_taxa_vector <- function(dist = clr_euk,
   # !!variable := name_of_col_from_df
   
   figure_ord %>% print() +
-    geom_segment(data = all, aes(x = 0, xend=Axis.1* fact, y=0, yend=Axis.2 * fact), arrow = arrow(length = unit(0.25, "cm")), colour = "grey10", lwd=0.3) + #add vector arrows of significant species
-    ggrepel::geom_text_repel(data = all, aes(x= Axis.1* fact, y=Axis.2*fact, label = tax_rank_plot), cex = 3, direction = "both", segment.size = 0.25) -> p2
+    geom_segment(data = all, 
+                 aes(x = 0, xend=Axis.1* fact, y=0, yend=Axis.2 * fact), arrow = arrow(length = unit(0.25, "cm")), colour = "grey10", lwd=0.3, inherit.aes = FALSE) + #add vector arrows of significant species
+    ggrepel::geom_text_repel(data = all, aes(x= Axis.1* fact, y=Axis.2*fact, label = tax_rank_plot), cex = 3, direction = "both", segment.size = 0.25, inherit.aes = FALSE) -> p2
   
   out <- list("plot" = p2,
               "ord" = iMDS,
