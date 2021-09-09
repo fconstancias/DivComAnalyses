8847a95e8239c5367b1e7cd58dc7a90917a814cf
fconstancias <florentinconstancias@gmail.com>
2021-09-09 16:33:51 +0200

    Update phyloseq_beta.R

--- R/phyloseq_beta.R
+++ R/phyloseq_beta.R
@@ -353,6 +353,7 @@ calc_pairwise_permanovas_strata <- function(dm, metadata_map, compare_header, n_
   comp_pairs = combn(levels(comp_var), 2)
   pval = c()
   R2 = c()
+  
   for (i in 1:ncol(comp_pairs)) {
     pair = comp_pairs[, i]
     dm_w_map = list(dm_loaded = dm, map_loaded = metadata_map)
@@ -363,29 +364,29 @@ calc_pairwise_permanovas_strata <- function(dm, metadata_map, compare_header, n_
     if (strat %in% colnames(metadata_map)){
       
       if (!missing(n_perm)) {
-        m = vegan::adonis(dm_w_map_filt$dm_loaded ~ dm_w_map_filt$map_loaded[,
+        m = vegan::adonis2(dm_w_map_filt$dm_loaded ~ dm_w_map_filt$map_loaded[,
                                                                              compare_header], permutations = n_perm,
                           strata = dm_w_map_filt$map_loaded[,
                                                             strat])
       }
       else {
-        m = vegan::adonis(dm_w_map_filt$dm_loaded ~ dm_w_map_filt$map_loaded[,
+        m = vegan::adonis2(dm_w_map_filt$dm_loaded ~ dm_w_map_filt$map_loaded[,
                                                                              compare_header],
                           strata = dm_w_map_filt$map_loaded[,
                                                             strat])
       }
     }else{
       if (!missing(n_perm)) {
-        m = vegan::adonis(dm_w_map_filt$dm_loaded ~ dm_w_map_filt$map_loaded[,
+        m = vegan::adonis2(dm_w_map_filt$dm_loaded ~ dm_w_map_filt$map_loaded[,
                                                                              compare_header], permutations = n_perm)
       }
       else {
-        m = vegan::adonis(dm_w_map_filt$dm_loaded ~ dm_w_map_filt$map_loaded[,
+        m = vegan::adonis2(dm_w_map_filt$dm_loaded ~ dm_w_map_filt$map_loaded[,
                                                                              compare_header])
       }
     }
-    pval = c(pval, m$aov.tab$`Pr(>F)`[1])
-    R2 = c(R2, m$aov.tab$R2[1])
+    pval = c(pval, m$`Pr(>F)`[1])
+    R2 = c(R2, m$R2[1])
   }
   results = data.frame(t(comp_pairs), R2, pval)
   results$pvalBon = pval * length(pval)
