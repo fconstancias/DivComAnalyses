d720d35e2546564c3330f003f0b12837f53f94ab
Florentin Constancias <florentinconstancias@gmail.com>
2024-11-21 15:49:27 +0100

    Update phyloseq_taxa_tests.R
    moved lefse_maaslin2_plot

--- R/phyloseq_taxa_tests.R
+++ R/phyloseq_taxa_tests.R
@@ -764,141 +764,7 @@ phyloseq_maaslin3 <- function(phyloseq,
   ##---------------------------------------------
   require(tidyverse); require(maaslin3); require(phyloseq)
   
-  ##---------------------------------------------
-  
-  run_make_coef_plot <- function(merged_results_sig, 
-                                 max_significance,
-                                 class) {
-    coef_plot_vars <- class
-    
-    coef_plot_data <- merged_results_sig[merged_results_sig$metadata %in% coef_plot_vars,]
-    
-    # Limit plotted coefficients to median +/- 10 times distance to quartiles
-    quantile_df <- coef_plot_data %>%
-      dplyr::group_by(.data$full_metadata_name) %>%
-      dplyr::summarise(
-        lower_q = median(.data$coef) - 10 * 
-          (median(.data$coef) - quantile(.data$coef, 0.25)),
-        upper_q = median(.data$coef) + 10 * 
-          (quantile(.data$coef, 0.75) - median(.data$coef))
-      ) %>%
-      data.frame()
-    rownames(quantile_df) <- quantile_df$full_metadata_name
-    
-    # Make sure insignificant coefficients don't distort the plot
-    coef_plot_data <-
-      coef_plot_data[coef_plot_data$qval_individual < 
-                       max_significance |
-                       (coef_plot_data$coef > quantile_df[
-                         coef_plot_data$full_metadata_name, 
-                         'lower_q'] &
-                          coef_plot_data$coef < quantile_df[
-                            coef_plot_data$full_metadata_name, 
-                            'upper_q']),]
-    
-    # Choose breaks for plot
-    custom_break_fun <- function(n) {
-      return(function(x) {
-        extended_breaks <- scales::breaks_extended(n)(x)
-        if (max(x) > 0) {
-          extended_breaks <- extended_breaks[
-            extended_breaks <= max(x) * 0.9]
-        } else {
-          extended_breaks <- extended_breaks[
-            extended_breaks <= max(x) * 1.1]
-        }
-        if (min(x) > 0) {
-          extended_breaks <- extended_breaks[
-            extended_breaks >= min(x) * 1.1]
-        } else {
-          extended_breaks <- extended_breaks[
-            extended_breaks >= min(x) * 0.9]
-        }
-        extended_breaks
-      })
-    }
-    
-    # Plot
-    p1 <- ggplot(coef_plot_data, aes(x = feature, y = coef, fill = value, alpha = model)) +
-      geom_bar(stat = "identity", 
-               position = position_dodge(width = 0.8), 
-               width = 0.7) +
-      geom_errorbar(aes(ymin = coef - stderr, ymax = coef + stderr),
-                    position = position_dodge(width = 0.8), 
-                    width = 0.25) +
-      scale_fill_brewer(palette = "Dark2") +
-      labs(x = "Feature", y = "Coefficient", fill = "Class", alpha = "Model") +
-      theme_bw() +
-      theme(axis.text.x = element_text(angle = 45, hjust = 1)) +
-      theme(panel.spacing.x = unit(0.5, "lines")) + 
-      coord_flip() + 
-      facet_wrap(
-        ~ value,
-        scales = 'free_x',
-        ncol = length(coef_plot_vars)
-      ) + 
-      scale_alpha_manual(values = c('Abundance' = 1, 'Prevalence' = 0.5)) + 
-      scale_y_continuous(
-        breaks = custom_break_fun(n = 6),
-        limits = c(
-          min(coef_plot_data$coef) - 
-            quantile(coef_plot_data$stderr, 0.8),
-          max(coef_plot_data$coef) + 
-            quantile(coef_plot_data$stderr, 0.8)
-        )
-      )
-    
-    return(p1)
-  }
-  
-  
-  run_maaslin_plotting <- function(fit_out, class, alpha_threshold, first_n = 20) {
-    fit_data_abundance <- fit_out$fit_data_abundance
-    fit_data_prevalence <- fit_out$fit_data_prevalence
-    
-    if (is.null(fit_data_abundance$results)) {
-      merged_results <- fit_data_prevalence$results
-    } else if (is.null(fit_data_prevalence$results)) {
-      merged_results <- fit_data_abundance$results
-    } else {
-      merged_results <- rbind(fit_data_abundance$results,
-                              fit_data_prevalence$results)
-    }
-    
-    merged_results <- maaslin3:::preprocess_merged_results(merged_results)
-    
-    # Subset associations for plotting
-    merged_results_joint_only <-
-      unique(merged_results[, c('feature', 'qval_joint')])
-    merged_results_joint_only <-
-      merged_results_joint_only[
-        order(merged_results_joint_only$qval_joint),]
-    if (length(unique(merged_results_joint_only$feature)) < first_n) {
-      first_n <- length(unique(merged_results_joint_only$feature))
-    }
-    signif_taxa <-
-      unique(merged_results_joint_only$feature)[seq(first_n)]
-    
-    merged_results_sig <- merged_results %>%
-      dplyr::filter(.data$feature %in% signif_taxa)
-    
-    p1 <- run_make_coef_plot(merged_results_sig = merged_results_sig, 
-                             max_significance = alpha_threshold, 
-                             class = class)
-    
-    height_out <-
-      5.5 + max(first_n / 5 - 5, 0) + nchar(as.character(class)) / 10
-    width_out <-
-      2 + max(nchar(merged_results$feature)) / 15
-    
-    ggplot2::ggsave(
-      filename = 'output/summary_plot.png',
-      plot = p1, 
-      dpi = 600,
-      width = width_out,
-      height = height_out)
-  }
-  ##---------------------------------------------
+
   
   maaslin3(input_data = phyloseq %>% otu_table() %>%  t() %>% data.frame() ,
            input_metadata = phyloseq %>% sample_data()  %>% data.frame(),
@@ -963,7 +829,141 @@ phyloseq_maaslin3 <- function(phyloseq,
   return(out)
 }
 
+##---------------------------------------------
 
+run_make_coef_plot <- function(merged_results_sig, 
+                               max_significance,
+                               class) {
+  coef_plot_vars <- class
+  
+  coef_plot_data <- merged_results_sig[merged_results_sig$metadata %in% coef_plot_vars,]
+  
+  # Limit plotted coefficients to median +/- 10 times distance to quartiles
+  quantile_df <- coef_plot_data %>%
+    dplyr::group_by(.data$full_metadata_name) %>%
+    dplyr::summarise(
+      lower_q = median(.data$coef) - 10 * 
+        (median(.data$coef) - quantile(.data$coef, 0.25)),
+      upper_q = median(.data$coef) + 10 * 
+        (quantile(.data$coef, 0.75) - median(.data$coef))
+    ) %>%
+    data.frame()
+  rownames(quantile_df) <- quantile_df$full_metadata_name
+  
+  # Make sure insignificant coefficients don't distort the plot
+  coef_plot_data <-
+    coef_plot_data[coef_plot_data$qval_individual < 
+                     max_significance |
+                     (coef_plot_data$coef > quantile_df[
+                       coef_plot_data$full_metadata_name, 
+                       'lower_q'] &
+                        coef_plot_data$coef < quantile_df[
+                          coef_plot_data$full_metadata_name, 
+                          'upper_q']),]
+  
+  # Choose breaks for plot
+  custom_break_fun <- function(n) {
+    return(function(x) {
+      extended_breaks <- scales::breaks_extended(n)(x)
+      if (max(x) > 0) {
+        extended_breaks <- extended_breaks[
+          extended_breaks <= max(x) * 0.9]
+      } else {
+        extended_breaks <- extended_breaks[
+          extended_breaks <= max(x) * 1.1]
+      }
+      if (min(x) > 0) {
+        extended_breaks <- extended_breaks[
+          extended_breaks >= min(x) * 1.1]
+      } else {
+        extended_breaks <- extended_breaks[
+          extended_breaks >= min(x) * 0.9]
+      }
+      extended_breaks
+    })
+  }
+  
+  # Plot
+  p1 <- ggplot(coef_plot_data, aes(x = feature, y = coef, fill = value, alpha = model)) +
+    geom_bar(stat = "identity", 
+             position = position_dodge(width = 0.8), 
+             width = 0.7) +
+    geom_errorbar(aes(ymin = coef - stderr, ymax = coef + stderr),
+                  position = position_dodge(width = 0.8), 
+                  width = 0.25) +
+    scale_fill_brewer(palette = "Dark2") +
+    labs(x = "Feature", y = "Coefficient", fill = "Class", alpha = "Model") +
+    theme_bw() +
+    theme(axis.text.x = element_text(angle = 45, hjust = 1)) +
+    theme(panel.spacing.x = unit(0.5, "lines")) + 
+    coord_flip() + 
+    facet_wrap(
+      ~ value,
+      scales = 'free_x',
+      ncol = length(coef_plot_vars)
+    ) + 
+    scale_alpha_manual(values = c('Abundance' = 1, 'Prevalence' = 0.5)) + 
+    scale_y_continuous(
+      breaks = custom_break_fun(n = 6),
+      limits = c(
+        min(coef_plot_data$coef) - 
+          quantile(coef_plot_data$stderr, 0.8),
+        max(coef_plot_data$coef) + 
+          quantile(coef_plot_data$stderr, 0.8)
+      )
+    )
+  
+  return(p1)
+}
+
+
+run_maaslin_plotting <- function(fit_out, class, alpha_threshold, first_n = 20) {
+  fit_data_abundance <- fit_out$fit_data_abundance
+  fit_data_prevalence <- fit_out$fit_data_prevalence
+  
+  if (is.null(fit_data_abundance$results)) {
+    merged_results <- fit_data_prevalence$results
+  } else if (is.null(fit_data_prevalence$results)) {
+    merged_results <- fit_data_abundance$results
+  } else {
+    merged_results <- rbind(fit_data_abundance$results,
+                            fit_data_prevalence$results)
+  }
+  
+  merged_results <- maaslin3:::preprocess_merged_results(merged_results)
+  
+  # Subset associations for plotting
+  merged_results_joint_only <-
+    unique(merged_results[, c('feature', 'qval_joint')])
+  merged_results_joint_only <-
+    merged_results_joint_only[
+      order(merged_results_joint_only$qval_joint),]
+  if (length(unique(merged_results_joint_only$feature)) < first_n) {
+    first_n <- length(unique(merged_results_joint_only$feature))
+  }
+  signif_taxa <-
+    unique(merged_results_joint_only$feature)[seq(first_n)]
+  
+  merged_results_sig <- merged_results %>%
+    dplyr::filter(.data$feature %in% signif_taxa)
+  
+  p1 <- run_make_coef_plot(merged_results_sig = merged_results_sig, 
+                           max_significance = alpha_threshold, 
+                           class = class)
+  
+  height_out <-
+    5.5 + max(first_n / 5 - 5, 0) + nchar(as.character(class)) / 10
+  width_out <-
+    2 + max(nchar(merged_results$feature)) / 15
+  
+  ggplot2::ggsave(
+    filename = 'output/summary_plot.png',
+    plot = p1, 
+    dpi = 600,
+    width = width_out,
+    height = height_out)
+}
+##---------------------------------------------
 
 #' @title ...
 #' @param .
